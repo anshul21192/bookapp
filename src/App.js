e3c51ea0c339bdbf7be2deeaec88e90d511f1798
anshul21192 <83779699+anshul21192@users.noreply.github.com>
2023-08-07 03:45:16 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SignIn from "./signIn/signIn";
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+    <SignIn></SignIn>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/signIn/signIn.js
@@ -0,0 +1,49 @@
+import "../signIn/signIn.css";
+import Container from "@mui/material/Container";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Button,
+  Checkbox,
+  Chip,
+  Divider,
+  Link,
+  TextField,
+} from "@mui/material";
+import { FcGoogle } from "react-icons/fc";
+
+function SignIn() {
+  return (
+    <Container maxWidth="sm" className="signIn">
+      <LockOutlinedIcon fontSize="large" />
+      <h1>Sign In</h1>
+      <div className="input_field">
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+      </div>
+      <Container>
+        <Checkbox defaultChecked />
+        Remember me
+      </Container>
+      <Container>
+        <Divider>
+          <Chip label="OR"></Chip>
+        </Divider>
+      </Container> 
+      <div className="third_party_signin">
+        <h2>
+        Sign In using google :          
+        </h2>
+        <FcGoogle size={30}></FcGoogle>
+      </div>
+      <Button variant="contained" className="signButton" >SIGN IN</Button>
+      <Container className="thirdSection" >
+        <Link href="https://www.example.com">Forgot Password?</Link>
+        <Link href="https://www.example.com">
+          Don't have an account?/sign up{" "}
+        </Link>
+      </Container>
+    </Container>
+  );
+}
+
+export default SignIn;
