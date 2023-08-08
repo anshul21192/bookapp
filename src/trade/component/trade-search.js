02e1266921965c7ff6f473a2d9a14396c7eca319
anshul21192 <83779699+anshul21192@users.noreply.github.com>
2023-08-09 01:54:56 +0530

    trade filter done

--- src/trade/component/trade-search.js
+++ src/trade/component/trade-search.js
@@ -1,15 +1,15 @@
 import "../trade.css";
 import * as React from 'react';
-function TradeSearch(){
+function TradeSearch(props){
     return    <div className="search_bar">
         <input type="search" placeholder="Search Trade here..." />
        <div>
         <span>Select Type of Trade</span>
-        <select name="" id="tradeStatus">
-          
+        <select name="" id="tradeStatus" onClick={(e)=>{props.setCurrentStatus(e.target.value)}}>
+          <option value={'all'}>All</option>
           <option value={"matured"}>Matured</option>
-          <option value="unmature">Un-Matured</option>
-          <option value="failed">Failed</option>
+          <option value={"unmature"}>Un-Matured</option>
+          <option value={"failed"}>Failed</option>
         </select>
        </div>
       </div>
